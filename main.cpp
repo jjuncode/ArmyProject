f93091e763eccaff223eef78f0957ff5a163bce9
jjuncode <141617473+jjuncode@users.noreply.github.com>
2024-12-25 05:29:30 +0000

    Set SFML using CodeSpace

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout <<"test"<<std::endl;
+    // Create a window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+
+    // Main loop
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Clear the window with a color
+        window.clear(sf::Color::Blue);
+
+        // Display everything
+        window.display();
+    }
+
+    return 0;
 }
