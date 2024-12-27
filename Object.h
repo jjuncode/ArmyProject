7fed3e786ea20e5679515564cb79e83eb9340a00
jjuncode <141617473+jjuncode@users.noreply.github.com>
2024-12-27 14:33:12 +0000

    Obj Not Hold Componet

--- Object.h
+++ Object.h
@@ -3,25 +3,13 @@
 
 class Object {
 private:
-	std::vector<std::unique_ptr<Component>> m_components;	// holding components
+//	std::vector<std::unique_ptr<Component>> m_components;	// Not Holding 
 	bool dead{ false };
 
 public:
 	void Dead() { dead = true; }
 	bool IsDead() { return dead; }
- 
-	virtual void Update(float dt){};
-	virtual void Render(){};
 
 	Object()
 	{}
-
-	virtual ~Object() {};
-
-public:
-	// =====================
-	// Object Template
-	// =====================
-	template<typename T> 
-	static std::unique_ptr<T> CreatePtr() {	return std::make_unique<T>();}
 };
