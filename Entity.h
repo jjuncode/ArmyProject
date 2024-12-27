4ca79578ab058d274c795ef2b5baf085510b3af6
jjuncode <141617473+jjuncode@users.noreply.github.com>
2024-12-27 14:42:14 +0000

    Entity Fill blank id

--- Entity.h
+++ Entity.h
@@ -4,22 +4,26 @@ class Entity {
 private:
 //	std::vector<std::unique_ptr<Component>> m_components;	// Not Holding 
 	bool dead{ false };
-	int id;
+	int m_id;
 	static int id_cnt;
 	static std::stack<int> remain_id;
 
 public:
-	void Dead() { dead = true; }
+	void Dead() 
+	{ 
+		dead = true;
+		remain_id.push(m_id);
+	}
 	bool IsDead() { return dead; }
 
 	Entity()
 	{
 		if ( !remain_id.size()){
 			// Is Empty
-			id = id_cnt++;
+			m_id = id_cnt++;
 		}
 		else{
-			id = remain_id.top();
+			m_id = remain_id.top();
 			remain_id.pop();
 		}		
 	}
