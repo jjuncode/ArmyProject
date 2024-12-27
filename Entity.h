a9e5f2388e81498343ebc6a98804334027705e5f
jjuncode <141617473+jjuncode@users.noreply.github.com>
2024-12-27 14:40:11 +0000

    Set Entity Id

--- Entity.h
+++ Entity.h
@@ -4,11 +4,23 @@ class Entity {
 private:
 //	std::vector<std::unique_ptr<Component>> m_components;	// Not Holding 
 	bool dead{ false };
+	int id;
+	static int id_cnt;
+	static std::stack<int> remain_id;
 
 public:
 	void Dead() { dead = true; }
 	bool IsDead() { return dead; }
 
 	Entity()
-	{}
+	{
+		if ( !remain_id.size()){
+			// Is Empty
+			id = id_cnt++;
+		}
+		else{
+			id = remain_id.top();
+			remain_id.pop();
+		}		
+	}
 };
